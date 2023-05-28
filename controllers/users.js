c8e943d0ceb3103baf239abeb03b1961ab97e204
MattB <104602457+mbencomo2@users.noreply.github.com>
2023-05-27 21:43:37 -0700

    - Deleting User uses correct method - Update User password using hash - Check for existing emails before making changes

--- controllers/users.js
+++ controllers/users.js
@@ -22,26 +22,12 @@ const getUser = async (req, res) => {
 const createUser = async (req, res) => {
   // #swagger.tags = ['Users']
   try {
-    const date = new Date();
-    const plainPassword = req.body.password;
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-      bcrypt.hash(plainPassword, salt, async (err, hash) => {
-        const user = new User({
-          name: req.body.name,
-          email: req.body.email,
-          dateJoined: date,
-          password: hash,
-          faveGenre: req.body.favGenre || "",
-          comment: req.body.comment || ""
-        });
-        const result = await user.save();
-        if (result) {
-          const userData = result.toObject();
-          delete userData.password;
-          res.status(201).json({ message: "User created successfully" });
-        }
-      });
-    });
+    const emailExists = await checkEmail(req.body.email);
+    if (emailExists) {
+      newUser(req, res);
+    } else {
+      res.status(422).json({ error: { message: "Email already exists" } });
+    }
   } catch (err) {
     console.log(err);
     res.status(404).json({ message: "Error creating user", error: `${err}` });
@@ -51,19 +37,11 @@ const createUser = async (req, res) => {
 const updateUser = async (req, res) => {
   // #swagger.tags = ['Users']
   try {
-    const user = await User.findById(req.id);
-    if (user) {
-      user.name = req.body.name || user.name;
-      user.email = req.body.email || user.email;
-      user.comment = req.body.comment || user.comment;
-      const result = await user.save();
-      if (result) {
-        const userData = result.toObject();
-        delete userData.password;
-        res.status(201).json({ message: "User updated successfully", user: userData });
-      }
+    const emailExists = await checkEmail(req.body.email);
+    if (emailExists) {
+      updUser(req, res);
     } else {
-      res.status(422).json({ message: "Cannot find user" });
+      res.status(422).json({ error: { message: "Email already exists" } });
     }
   } catch (err) {
     console.log(err);
@@ -76,7 +54,7 @@ const deleteUser = async (req, res) => {
   try {
     User.findById(req.id).then((user) => {
       if (user) {
-        User.remove({ _id: req.id }).then(
+        User.deleteOne({ _id: req.id }).then(
           res.status(202).json({ message: "User deleted successfully" })
         );
       } else {
@@ -114,6 +92,66 @@ const login = async (req, res) => {
 
 const createJWT = (id) => {
   return jwt.sign({ id: id }, process.env.TOKEN_SECRET, { expiresIn: "1800s" });
+};
+
+const checkEmail = async (email) => {
+  const result = await User.findOne({ email: email });
+  if (result) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const newUser = async (req, res) => {
+  const date = new Date();
+  const plainPassword = req.body.password;
+
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    bcrypt.hash(plainPassword, salt, async (err, hash) => {
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        dateJoined: date,
+        password: hash,
+        faveGenre: req.body.favGenre || "",
+        comment: req.body.comment || ""
+      });
+
+      const result = await user.save();
+
+      if (result.isModified()) {
+        const userData = result.toObject();
+        delete userData.password;
+        res.status(201).json({ message: "User created successfully" });
+      }
+    });
+  });
+};
+
+const updUser = async (req, res) => {
+  const user = await User.findById(req.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.comment = req.body.comment || user.comment;
+    if (req.body.password) {
+      user.password = 
+        await bcrypt
+          .genSalt(saltRounds)
+          .then(async (salt, err) => await bcrypt.hash(req.body.password, salt));
+    }
+    const result = await user.save();
+    if (result) {
+      const userData = result.toObject();
+      delete userData.password;
+      res.status(202).json({ message: "User updated successfully", user: userData });
+    } else {
+      res.status(422).json({ error: { message: "Could not update user info" } });
+    }
+  } else {
+    res.status(422).json({ message: "Cannot find user" });
+  }
 };
 
 module.exports = { getUser, createUser, updateUser, deleteUser, login };
